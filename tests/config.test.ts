b86ef7036701820dfe15da5e5f2f2f3a70c29aef
Christophe Tiraoui <krux@krux.fr>
2023-02-04 06:29:03 +0100

    fix

--- tests/config.test.ts
+++ tests/config.test.ts
@@ -5,11 +5,15 @@ describe("config testing", () => {
     process.env.K2_ENV = "test";
     process.env.K2_INVENTORY = path.join(__dirname, "fakes");
 
-    const config = loadConfig();
+    const config = loadConfig({
+      my: { custo: "abc" },
+    });
     expect(config).not.toBeUndefined();
     expect(config.get("global.config.key")).toBe("value30");
     expect(config.get("specific.env.key")).toBe("value1");
     expect(config.get("group1-component1.custom")).toBe("aloha");
+    expect(config.get("my.custo")).toBe("abc");
+
     expect(config.getOrDefault("inexistant", "unknow")).toBe("unknow");
   });
 });
